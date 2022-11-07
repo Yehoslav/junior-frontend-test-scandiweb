aa540ab0ec31a4aa872000abc58cb941c491a7bd
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-07 19:52:51 +0200

    Added loading style for the item component

--- src/components/cart-item/CartItem.jsx
+++ src/components/cart-item/CartItem.jsx
@@ -1,21 +1,46 @@
-import {Component} from 'react';
+import { Component } from "react";
 
-import './cart-item.scss';
+import "./cart-item.scss";
 import placeholder from "../../res/img/placeholder.png";
 
 class CartItem extends Component {
+  state = {
+    // WARN: Default should be true
+    loading: false,
+  };
+
   render() {
+    if (this.state.loading) {
+      return (
+        <div className="row-g4 load">
+          <div className="atts">
+            <div className="text loading"></div>
+            <div className="text loading"></div>
+            <div className="text loading"></div>
+            <div className="row-g8">
+              <div className="btn-sm loading"></div>
+              <div className="btn-sm loading"></div>
+              <div className="btn-sm loading"></div>
+              <div className="btn-sm loading"></div>
+            </div>
+          </div>
+          <div className="col sb">
+            <div className="btn-sm loading"></div>
+            <div className="btn-sm loading"></div>
+          </div>
+          <div className="image loading"></div>
+        </div>
+      );
+    }
+
     return (
       <div className="row-g4">
-
         <div className="attributes">
           <div className="font-l">Brand name</div>
           <div className="font-l">Item name</div>
           <div className="font-sb">$50.00</div>
           <div>
-            <div className="font-ty">
-              Size:
-            </div>
+            <div className="font-ty">Size:</div>
             <div className="row-g8">
               <button className="btn-sm btn__sec">XS</button>
               <button className="btn-sm btn__sec">S</button>
@@ -39,7 +64,6 @@ class CartItem extends Component {
             alt="Image placeholder"
           />
         </div>
-
       </div>
     );
   }
