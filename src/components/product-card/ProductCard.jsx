576edc048621d094536d69a35f9ef6f55b81e9df
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-08 18:09:28 +0200

    Added the 'add to cart' button

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,22 +1,24 @@
-import {Component} from 'react';
+import { Component } from "react";
 
-import './product-card.scss';
+import "./product-card.scss";
 import placeholder from "../../res/img/placeholder.png";
+import {CartIcon} from '../mini-cart';
 
-class ItemCard extends Component {
+class ProductCard extends Component {
   render() {
     return (
-       <div className="card">
-        <div className="image"><img src={placeholder} alt="" /></div>
-        <div className="font-l">
-          Appolo something there
+      <div className="card">
+        <div className="image">
+          <img src={placeholder} alt="" />
+          <button className="add-btn">
+            <CartIcon />
+          </button>
         </div>
-          <div className="font-m">
-          $500.00
-        </div>
+        <div className="font-l">Appolo something there</div>
+        <div className="font-m">$500.00</div>
       </div>
     );
   }
 }
 
-export default ItemCard;
+export default ProductCard;
