d4aa51de8dc7c5af432d617d81d68c817fe2963e
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-08 19:24:52 +0200

    Made product name into a link to the product example page

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,7 +1,9 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 
 import "./product-card.scss";
 import placeholder from "../../res/img/placeholder.png";
+
 import {CartIcon} from '../mini-cart';
 
 class ProductCard extends Component {
@@ -14,7 +16,7 @@ class ProductCard extends Component {
             <CartIcon />
           </button>
         </div>
-        <div className="font-l">Appolo something there</div>
+        <Link to="/product/example" className="font-l">Appolo something there</Link>
         <div className="font-m">$500.00</div>
       </div>
     );
