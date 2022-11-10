5fda522ff15836eba4bf3774dc38a1d1464d31ca
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-10 17:32:02 +0200

    Updated the alt to Placeholder and formated the file

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -4,19 +4,20 @@ import { Link } from "react-router-dom";
 import "./product-card.scss";
 import placeholder from "../../res/img/placeholder.png";
 
-import {CartIcon} from '../mini-cart';
+import { CartIcon } from '../mini-cart';
 
 class ProductCard extends Component {
   render() {
     return (
       <div className="card">
         <div className="image">
-          <img src={placeholder} alt="" />
+          <img src={placeholder} alt="Placeholder" />
+        </div>
           <button className="add-btn">
             <CartIcon />
           </button>
-        </div>
-        <Link to="/product/example" className="fp-l">Appolo something there</Link>
+        <Link to="/product/example" className="fp-l">
+          Appolo something there</Link>
         <div className="fp-m">$500.00</div>
       </div>
     );
