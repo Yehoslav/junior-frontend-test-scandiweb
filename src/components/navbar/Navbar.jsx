a509de90f054aeb70e6bf1b492e1b6d7608285de
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-10 23:52:17 +0200

    Wrapped the navbar content into a div.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,22 +13,24 @@ class Navbar extends Component {
 
     return (
       <nav>
-        <div className="link-group">
-          <NavLink className="link" to="/store/all">
-            <span>All</span>
-          </NavLink>
-          <NavLink className="link" to="/store/clothes">
-            <span>Clothes</span>
-          </NavLink>
-          <NavLink className="link" to="/store/tech">
-            <span>Tech</span>
-          </NavLink>
-        </div>
-        <img className="brand" src={logo} alt="Site logo" />
-        <div className="actions">
-{/* // FIXME: Make dropdown into a general element that could warp other elements */}
-          <CurrencySelector />
-          <MiniCart />
+        <div className="navbar">
+          <div className="link-group">
+            <NavLink className="link" to="/store/all">
+              <span>All</span>
+            </NavLink>
+            <NavLink className="link" to="/store/clothes">
+              <span>Clothes</span>
+            </NavLink>
+            <NavLink className="link" to="/store/tech">
+              <span>Tech</span>
+            </NavLink>
+          </div>
+          <img className="brand" src={logo} alt="Site logo" />
+          <div className="actions">
+          {/** FIXME: Make dropdown into a general element that could warp other elements */}
+            <CurrencySelector />
+            <MiniCart />
+          </div>
         </div>
       </nav>
     );
