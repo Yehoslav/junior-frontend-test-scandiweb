29df88fa48cfd6148be94a50c335542981d9fd74
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-10 12:49:17 +0200

    Created a Cart page

---
+++ src/components/cart-view/CartView.jsx
@@ -0,0 +1,41 @@
+import {Component} from 'react';
+import CartItem from '../cart-item';
+
+import './cart-view.scss';
+
+class CartView extends Component {
+  render() {
+    return (
+      <div className="col-g8">
+        <h1>Cart</h1>
+        <hr 
+         style={{
+            color: "red",
+            backgroundColor: "green",
+            height: 5
+          }}
+          />
+        <CartItem />
+        <hr />
+        <CartItem />
+        <hr />
+        <CartItem />
+        <hr />
+        <div className="col-g8 hug">
+          <div className="grid f-lg">
+            <div>Tax 21%:</div>
+            <div className='fp-b'>$20</div>
+            <div>Quantity:</div>
+            <div className='fp-b'>3</div>
+            <div>Total:</div>
+            <div className='fp-b'>$100</div>
+          </div>
+          <button className='btn__pri btn-lg f-ty ws'>Order</button>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default CartView;
