8fad163ef21d1df6d8c49d8b3f630e390d68d1ab
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-11 15:56:18 +0200

    Query products from the server.

--- src/components/store-view/StoreView.jsx
+++ src/components/store-view/StoreView.jsx
@@ -1,24 +1,80 @@
-import {Component} from 'react';
+import { Component } from "react";
 
-import './store-view.scss';
-import withRouter from '../../lib/withRouter';
+import "./store-view.scss";
+import withRouter from "../../lib/withRouter";
 
-import ProductCard from '../product-card';
+import ProductCard from "../product-card";
+
+import { 
+  client, 
+  // CombinedField, 
+  Field, 
+  Query 
+} from "@tilework/opus";
+
+client.setEndpoint("http://localhost:4000");
+
+const productFields = ["id", "name", "gallery"];
+
+const createQuery = (category) => {
+  return new Query("category", false)
+    .addArgument("input", "CategoryInput", { title: category })
+    .addField(
+      new Field("products", true)
+        .addFieldList(productFields)
+        .addField(
+          new Field("prices", true)
+            .addField(new Field("currency").addField("symbol"))
+            .addField("amount")
+        )
+    );
+};
 
 class StoreView extends Component {
+  state = {
+    products: [],
+    loading: true,
+  };
+
+  getData = (category) => {
+    client
+      .post(createQuery(category))
+      .then((res) => this.setState({ products: res.category.products }));
+  };
+
+  componentDidMount() {
+    const { category } = this.props.router.params;
+    this.getData(category);
+    this.setState({ loading: false });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { category } = this.props.router.params;
+    const prevCategory = prevProps.router.params.category;
+    // const {loading} = this.state
+
+    if (category !== prevCategory) {
+      client
+        .post(createQuery(category))
+        .then((res) => this.setState({ products: res.category.products }));
+    }
+  }
+
   render() {
-    const {params: {category}} = this.props.router
+    console.log("Render store view");
+    const {
+      params: { category },
+    } = this.props.router;
+
+    const productCards = this.state.products.map((product) => (
+      <ProductCard key={product.id} {...product} />
+    ));
+
     return (
       <>
         <h1>{category}</h1>
-      <div className="row-g12 wrap">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-      </div>
-        </>
+        <div className="row-g12 wrap">{productCards}</div>
+      </>
     );
   }
 }
