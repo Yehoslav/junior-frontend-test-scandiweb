267e29556137a0b15372a07da102cf89ad40415e
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-17 21:32:17 +0200

    Using redux to get the products.

--- src/components/store-view/StoreView.jsx
+++ src/components/store-view/StoreView.jsx
@@ -1,82 +1,65 @@
 import { Component } from "react";
+import { connect } from "react-redux";
 
 import "./store-view.scss";
 import withRouter from "../../utils/withRouter";
+import { fetchProducts } from "../../lib/storeSlice";
 
 import ProductCard from "../product-card";
 
-import { 
-  client, 
-  // CombinedField, 
-  Field, 
-  Query 
-} from "@tilework/opus";
-
-client.setEndpoint("http://localhost:4000");
-
-const productFields = ["id", "name", "gallery"];
-
-const createQuery = (category) => {
-  return new Query("category", false)
-    .addArgument("input", "CategoryInput", { title: category })
-    .addField(
-      new Field("products", true)
-        .addFieldList(productFields)
-        .addField(
-          new Field("prices", true)
-            .addField(new Field("currency").addField("symbol"))
-            .addField("amount")
-        )
-    );
-};
-
 class StoreView extends Component {
-  state = {
-    products: [],
-    loading: true,
-  };
-
-  getData = (category) => {
-    client
-      .post(createQuery(category))
-      .then((res) => this.setState({ products: res.category.products }));
-  };
 
   componentDidMount() {
-    const { category } = this.props.router.params;
-    this.getData(category);
-    this.setState({ loading: false });
+    const {
+      router: {params: { category }},
+      getProducts
+    } = this.props;
+
+    getProducts(category);
   }
 
   componentDidUpdate(prevProps) {
-    const { category } = this.props.router.params;
+    const {
+      router: {params: { category }},
+      getProducts
+    } = this.props;
     const prevCategory = prevProps.router.params.category;
-    // const {loading} = this.state
 
     if (category !== prevCategory) {
-      client
-        .post(createQuery(category))
-        .then((res) => this.setState({ products: res.category.products }));
+      getProducts(category)
     }
   }
 
   render() {
-    console.log("Render store view");
     const {
-      params: { category },
-    } = this.props.router;
+      router: {params: { category }},
+      products,
+    } = this.props;
 
-    const productCards = this.state.products.map((product) => (
+    const productCards = products.map((product) => (
       <ProductCard key={product.id} {...product} />
     ));
 
     return (
       <>
         <h1>{category}</h1>
+        {/* FIXME: Make the component responsive */}
         <div className="row-g12 wrap">{productCards}</div>
       </>
     );
   }
 }
 
-export default withRouter(StoreView);
+const mapStateToProps = (state) => {
+  return {
+    products: state.store.products,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProducts: (category) => dispatch(fetchProducts(category))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(StoreView));
