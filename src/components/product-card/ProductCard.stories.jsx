952c91cf3a6bc46fb4d428066dae8c03370b4b17
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-18 20:42:51 +0200

    Renamed the story.

---
+++ src/components/product-card/ProductCard.stories.jsx
@@ -0,0 +1,22 @@
+import ProductCard from "./ProductCard";
+import placeholder from "../../res/img/placeholder.png";
+
+export default {
+  component: ProductCard,
+  title: 'ProductCart',
+}
+
+const Template = args => <ProductCard {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+  id: 'apollo-something-there',
+  name: 'Apollo Something THere',
+  gallery: [placeholder],
+  prices: [{
+    amount: 500,
+    currency: {
+      symbol: "$"
+    }
+  }]
+}
