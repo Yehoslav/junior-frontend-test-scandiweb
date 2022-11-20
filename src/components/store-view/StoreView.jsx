d38c4f9ab1e8ff147150c646984f1a90770c31f4
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-20 15:31:55 +0200

    Create the product list in a separate method.

--- src/components/store-view/StoreView.jsx
+++ src/components/store-view/StoreView.jsx
@@ -5,14 +5,15 @@ import "./store-view.scss";
 import withRouter from "../../utils/withRouter";
 import { fetchProducts } from "../../lib/storeSlice";
 
-import ProductCard from "../product-card";
+import ProductCard, { ProductCardLoading } from "../product-card";
 
 class StoreView extends Component {
-
   componentDidMount() {
     const {
-      router: {params: { category }},
-      getProducts
+      router: {
+        params: { category },
+      },
+      getProducts,
     } = this.props;
 
     getProducts(category);
@@ -20,46 +21,71 @@ class StoreView extends Component {
 
   componentDidUpdate(prevProps) {
     const {
-      router: {params: { category }},
-      getProducts
+      router: { params: { category }, },
+      getProducts,
     } = this.props;
     const prevCategory = prevProps.router.params.category;
 
-    if (category !== prevCategory) {
-      getProducts(category)
-    }
+    if (category !== prevCategory) getProducts(category);
   }
+
+  buildProductList = (products, status, error) => {
+    console.log(`Product list status: ${status}`);
+    switch (status) {
+      case "loading":
+        return (
+          <>
+            <ProductCardLoading />
+            <ProductCardLoading />
+            <ProductCardLoading />
+          </>
+        );
+      case "succeeded":
+        return products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ));
+      case "failed":
+        return <h3>{error}</h3>;
+      default:
+        return <h3>{status}</h3>;
+    }
+  };
 
   render() {
     const {
-      router: {params: { category }},
+      router: {
+        params: { category },
+      },
+      status,
+      error,
       products,
     } = this.props;
 
-    const productCards = products.map((product) => (
-      <ProductCard key={product.id} {...product} />
-    ));
+    const productCards = this.buildProductList(products, status, error);
 
     return (
-      <>
-        <h1>{category}</h1>
+      <div>
+        <p className="mt80 mb80 fp f-42 t__cap">{category}</p>
         {/* FIXME: Make the component responsive */}
         <div className="row-g12 wrap">{productCards}</div>
-      </>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    products: state.store.products,
-  }
-}
+const mapStateToProps = ({ store: { products, status, error } }) => ({
+  products,
+  status,
+  error,
+});
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getProducts: (category) => dispatch(fetchProducts(category))
-  }
-}
+    getProducts: (category) => dispatch(fetchProducts(category)),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(StoreView));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(StoreView));
