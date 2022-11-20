d874fb6d9a810b1ac0ca52610771cce93b460d93
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-20 17:39:52 +0200

    Changed the currencySlice to fetch the currencies.

--- src/lib/currencySlice.js
+++ src/lib/currencySlice.js
@@ -1,22 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getCurrencies } from "./database";
 
 const initialState = {
-  symbol: "$"
-}
+  globalCurrency: { symbol: "$", label: "USD" },
+  currencies: [],
+  status: "idle",
+  error: null,
+};
+
+/* Creates an asyncThunk to fetch tasks from a remote endpoint.
+ * You can read more about Redux Toolkit's thunks in the docs:
+ * https://redux-toolkit.js.org/api/createAsyncThunk
+ */
+export const fetchCurrencies = createAsyncThunk(
+  "store/getCurrencies",
+  async () => await getCurrencies()
+);
 
 const currencySlice = createSlice({
-  name: 'currency',
+  name: "currency",
   initialState,
   reducers: {
     updateCurrency(state, action) {
       return {
         ...state,
-        symbol: action.payload
-      }
-    }
+        globalCurrency: action.payload,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCurrencies.fulfilled, (state, action) => ({
+        ...state, 
+        status: "succeeded",
+        currencies: action.payload.currencies,
+      }))
+      .addCase(fetchCurrencies.pending, (state) => ({
+        ...state, 
+        status: "loading",
+        currencies: [],
+      }))
+      .addCase(fetchCurrencies.rejected, (state) => ({
+        ...state, 
+        status: "failed",
+        currencies: [],
+        // HACK: return a more explicit error message.
+        error: "Something went wrong"
+      }))
   }
-})
+});
 
-export const {updateCurrency} = currencySlice.actions;
+export const { updateCurrency } = currencySlice.actions;
 
 export default currencySlice.reducer;
