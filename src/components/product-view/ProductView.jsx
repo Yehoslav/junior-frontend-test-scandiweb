83629683229a6ff43ada7b4840d862c1e1d9c2a1
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-21 22:20:33 +0200

    Connected ProductView to redux.

--- src/components/product-view/ProductView.jsx
+++ src/components/product-view/ProductView.jsx
@@ -4,60 +4,117 @@ import "./product-view.scss";
 import placeholder from "../../res/img/placeholder.png";
 
 import AttributeSelector from "../attribute-selector/AttributeSelector";
+import  withRouter  from "../../utils/withRouter";
+import { fetchProduct, selectAttribute } from "../../lib/productSlice";
+import { fetchProduct as fetchAndAddToCart } from "../../lib/cartSlice";
+import { connect } from "react-redux";
 
 class ProductView extends Component {
-  render() {
-    return (
+  state = {
+    focusImg: placeholder,
+  }
+
+  selectImage = (src) => {
+    this.setState({focusImg: src})
+  }
+
+  componentDidMount = () => {
+    const { getProductData, router: { params: {productId}} } = this.props;
+    getProductData(productId)
+  }
+
+  buildContent = ({ gallery, id, brand, name, description, attributes, prices, }, onAddToCart, currency, selectAttr) => {
+    let { focusImg } = this.state;
+
+    // HACK: There should be a better method to set a default focused image
+    if (focusImg === placeholder || gallery.filter((item) => item === focusImg).length === 0) {
+      // this.selectImage(gallery[0])
+      focusImg = gallery[0]
+    }
+
+    console.dir(prices)
+   return (
       <div className="row-g8 wrap pt80">
         <div className="row">
           <div className="col-g8 preview">
-            <div className="prd-thumb">
-              <img src={placeholder} alt="Placeholder" />
-            </div>
-
-            <div className="prd-thumb">
-              <img src={placeholder} alt="Placeholder" />
-            </div>
-
-            <div className="prd-thumb">
-              <img src={placeholder} alt="Placeholder" />
-            </div>
+            {
+              gallery.map(img => (
+                <div 
+                  key={img}
+                  onClick={() => this.selectImage(img)}
+                  className="prd-thumb">
+                  <img src={img} alt={img} />
+                </div>
+              ))
+            }
           </div>
           <div className="img">
             <div className="prod-img">
-              <img src={placeholder} alt="Placeholder" />
+              <img src={focusImg} alt="Placeholder" />
             </div>
           </div>
         </div>
         <div className="col-g4 info">
-          <div className="fp-b f-30 pb5">Apollo</div>
-          <div className="fp f-30 pb10">Something more</div>
-          <AttributeSelector
-            title="title:"
-            attributes={["XL", "L", "M", "S"]}
+          <div className="fp-b f-30 pb5">{brand}</div>
+          <div className="fp f-30 pb10">{name}</div>
+          {attributes.map(attr => (
+          <AttributeSelector 
+            key={attr.name}
+            title={attr.name}
+            selected={attr.selectedAttr}
+            color={attr.type === "swatch"}
+            onAttributeSelect={(value) => {
+                const payload = {attrId: attr.name,  value}
+                console.log("Select attribute payload")
+                console.dir(payload)
+                selectAttr(payload)
+              }}
+            attributes={attr.items.map((item) => item.value)}
           />
-          <AttributeSelector
-            title="color:"
-            color={true}
-            attributes={["red", "yellow", "green", "gray"]}
-          />
+          ))}
           <div className="fp-b mt20" >
             PRICE:
           </div>
-          <div className="fp-b f-24">$50.00</div>
-          <button className="btn__pri f-16 p16 mt25" style={{ height: 50 }}>
+          <div className="fp-b f-24">
+            {currency.symbol}
+            {prices.find((item) => item.currency.symbol === currency.symbol).amount}
+          </div>
+          <button 
+            onClick={() => onAddToCart(id)}
+            className="btn btn__pri f-16 p16 mt25" 
+            style={{ height: 50 }}>
             ADD TO CART
           </button>
-          <div className="f-16 mt30">
-            Elit nostrum ab nam rem recusandae. Impedit tempora et voluptas
-            quidem perspiciatis? Reiciendis impedit quia ea nostrum accusamus?
-            Adipisci voluptatum accusantium et nemo nostrum In quia dolore eius
-            sequi deleniti
-          </div>
+          <div className="f-16 mt30" dangerouslySetInnerHTML={{__html: description}}></div>
         </div>
       </div>
-    );
+    )
+  }
+
+  render() {
+    console.log("ProductView")
+    console.dir(this.props)
+    
+    const {product, status, addToCart, currency, selectAttr} = this.props
+
+    return (status === "succeeded") ? this.buildContent(product, addToCart, currency, selectAttr) : <h1>loading</h1>;
   }
 }
 
-export default ProductView;
+const mapStateToProps = (state) => {
+  return {
+    product: state.product.productData,
+    status: state.product.status,
+    currency: state.currency.globalCurrency,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    selectAttr: (attrId, value) => dispatch(selectAttribute(attrId, value)),
+    getProductData: (productId) => dispatch(fetchProduct(productId)),
+    addToCart: (productId) => dispatch(fetchAndAddToCart(productId)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ProductView));
