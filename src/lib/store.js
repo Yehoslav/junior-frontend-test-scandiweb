23bcffb3bdcab0e0cae00d7b6ab6852c188333db
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-22 14:14:07 +0200

    Added a slice for the product view.

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import currencySlice from "./currencySlice";
 import cartSlice from "./cartSlice";
 import storeSlice from "./storeSlice";
+import productSlice from "./productSlice";
 
 export const store = configureStore({
   reducer: {
     currency: currencySlice,
     cart: cartSlice,
     store: storeSlice,
+    product: productSlice,
   }
 })
