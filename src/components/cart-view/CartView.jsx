f96f570e3f446a8fa008a95ff9c382fb8570b8d8
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-23 14:54:16 +0200

    Formatted the file.

--- src/components/cart-view/CartView.jsx
+++ src/components/cart-view/CartView.jsx
@@ -1,20 +1,17 @@
-import {Component} from 'react';
-import CartItem from '../cart-item';
-import { connect } from 'react-redux';
+import { Component } from "react";
+import CartItem from "../cart-item";
+import { connect } from "react-redux";
 import {
   increaseProductAmmount,
   decreaseProductAmmount,
 } from "../../lib/cartSlice";
 import { round, totalPrice } from "../../utils/funcs.js";
 
-import './cart-view.scss';
+import "./cart-view.scss";
 
 class CartView extends Component {
   render() {
-    const {
-      symbol,
-      products,
-    } = this.props;
+    const { symbol, products } = this.props;
     const { increaseAmount, decreaseAmount } = this.props;
 
     const items =
@@ -23,7 +20,7 @@ class CartView extends Component {
           <>
             <CartItem
               key={item.id}
-              { ...item}
+              {...item}
               price={item.prices.find(
                 (price) => price.currency.symbol === symbol
               )}
@@ -39,26 +36,36 @@ class CartView extends Component {
 
     return (
       <div className="col-g8">
-        <h1 className='t_upper fp-b f-32'>Cart</h1>
+        <h1 className="t_upper fp-b f-32">Cart</h1>
         <hr />
         {items}
         <div className="col-g8 hug">
           <div className="grid f-24">
             <div>Tax 21%:</div>
-            <div className='fp-b'>{symbol}{round(totalPrice(products, symbol)*0.21, 2)}</div>
+            <div className="fp-b">
+              {symbol}
+              {round(totalPrice(products, symbol) * 0.21, 2)}
+            </div>
             <div>Quantity:</div>
-            <div className='fp-b'>{products.reduce((previous, current) => {
-              return previous + current.amount
-            }, 0)}</div>
+            <div className="fp-b">
+              {products.reduce((previous, current) => {
+                return previous + current.amount;
+              }, 0)}
+            </div>
             <div>Total:</div>
-            <div className='fp-b'>{symbol}{totalPrice(products, symbol)}</div>
+            <div className="fp-b">
+              {symbol}
+              {totalPrice(products, symbol)}
+            </div>
           </div>
-          <button 
-            style={{height: 43}}
-            className='btn btn__pri f-14 t__upper ws'>Order</button>
+          <button
+            style={{ height: 43 }}
+            className="btn btn__pri f-14 t__upper ws"
+          >
+            Order
+          </button>
         </div>
       </div>
-      
     );
   }
 }
@@ -67,8 +74,7 @@ const mapStateToProps = (state) => ({
   products: state.cart.products,
   status: state.cart.status,
   symbol: state.currency.globalCurrency.symbol,
-})
-
+});
 
 const mapDispatchToProps = (dispatch) => {
   return {
