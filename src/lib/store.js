4bdb4ebe836739f610fadd419e5bdbe809c77f96
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-24 22:04:47 +0200

    Removed the product slice from the store.

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import currencySlice from "./currencySlice";
 import cartSlice from "./cartSlice";
 import storeSlice from "./storeSlice";
-import productSlice from "./productSlice";
 
 export const store = configureStore({
   reducer: {
     currency: currencySlice,
     cart: cartSlice,
     store: storeSlice,
-    product: productSlice,
   }
 })
