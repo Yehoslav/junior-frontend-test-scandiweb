1d30ace2f39172d5f4a555bdc8dfc7ffd10d028a
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-28 15:08:31 +0200

    Added router support and a mock for graphql

--- src/components/navbar/Navbar.stories.jsx
+++ src/components/navbar/Navbar.stories.jsx
@@ -1,20 +1,40 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import Navbar from ".";
+import { store } from "../../lib/store";
+import { Provider } from "react-redux";
+import { graphql } from "msw";
 
 export default {
   component: Navbar,
-  title: 'Navbar',
-  decorators: [withRouter],
-}
+  title: "Navbar",
+  decorators: [
+    (story) => <Provider store={store}>{story()}</Provider>,
+    withRouter,
+  ],
+  parameters: {
+    reactRouter: {
+      routePath: "/store/:category",
+      routeParams: { category: "all" },
+    },
+  },
+};
 
-const Template = args => <Navbar {...args} />;
+const Template = (args) => <Navbar {...args} />;
 
-export const Default = Template.bind({})
-Default.args = {
-  rotated: false,
-}
-
-export const ActiveCurrency = Template.bind({})
-ActiveCurrency.args = {
-  rotated: true,
-}
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      graphql.operation(async (req, res, ctx) => {
+        return res(
+          ctx.data({
+            currencies: [
+              { symbol: "$", label: "USD" },
+              { symbol: "€", label: "EUR" },
+            ],
+          })
+        );
+      }),
+    ],
+  },
+};
