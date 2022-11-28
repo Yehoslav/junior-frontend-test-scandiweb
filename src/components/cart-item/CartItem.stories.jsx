5305d6d07378155848345388ff4667f9a1c204e4
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-11-28 15:05:30 +0200

    Created stories for CartItem component.

---
+++ src/components/cart-item/CartItem.stories.jsx
@@ -0,0 +1,39 @@
+import CartItem from './CartItem';
+import { store } from "../../lib/store";
+import { Provider } from "react-redux";
+
+import placeholder from "../../res/img/placeholder.png";
+
+export default {
+  component: CartItem,
+  title: "CartItem",
+  decorators: [(story) => <Provider store={store}><div style={{padding: "2rem"}}>{story()}</div></Provider>],
+}
+
+const Template = (args) => <CartItem {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  id: "papuci",
+  name: "Papuci",
+  brand: "Moldoveneshti",
+  inMinicart: true,
+  amount: 2,
+  gallery: [placeholder],
+  attributes: [
+    {
+    name: "Size",
+    selectedAttr: "black",
+    type: "swatch",
+    items: [{value: "black"},{value: "brown"},{value: "white"}],
+  },{
+    name: "Color",
+    selectedAttr: "black",
+    type: "swatch",
+    items: [{value: "black"},{value: "brown"},{value: "white"}],
+  }],
+  price: {
+    currency: {symbol: "$"},
+    amount: 50
+  }
+}
