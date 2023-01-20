c53752c1d585dfb5bba80121a8599b14ee94014a
Yehoslav <yehoslavrudenco@gmail.com>
2023-01-20 16:15:36 +0200

    Formated router the file.

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -1,5 +1,10 @@
 import {redirect} from "react-router-dom"
+import ErrorPage from "./error"
 
 export function action () {
   return redirect("/store/all")
 }
+
+export {
+  ErrorPage
+};
--- src/components/routes/router.jsx
+++ src/components/routes/router.jsx
@@ -1,35 +1,16 @@
 import {
   createBrowserRouter,
-  useRouteError 
 } from "react-router-dom";
 
-import {action as indexAction} from '.';
+import {
+  action as indexAction,
+  ErrorPage
+} from '.';
 
 import CartView from "../cart-view";
 import ProductView from "../product-view";
 import StoreView from "../store-view";
 import App from "../app/App";
-
-function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-    }}>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
-}
 
 const router = createBrowserRouter ([
   {
